4f50e49da90d3637919593674259dcf21b784dc4
SimonaIvanova7 <tydi18@abv.bg>
2018-11-06 15:52:23 +0200

    6

---
+++ W3InClass/week6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace week6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Which bus is arrived?");
+                int busNumber = int.Parse(Console.ReadLine());
+
+                switch (busNumber)
+                {
+                    case "99":
+                        Console.WriteLine();
+                        break;
+                    case "44":
+                        Console.WriteLine();
+                        break;
+                    case "20":
+                        Console.WriteLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Fuck");
+                        break;
+                }
+
+                /*
+                if (busNumber == 99)
+                {
+                    Console.WriteLine("I will take it:");
+                }
+                else if (busNumber == 44)
+                {
+                    Console.WriteLine("Okey, this bus will stop 5 mins before your home.");
+                 else if (busNumber == 20)
+                    {
+                        Console.WriteLine("Okey, this bus will stop 15 mins before your home.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I will wait another");
+                }
+                catch (Exception ex)
+               {
+                Console.WriteLine($"{ex.Message});
+                }
+            }
+                 */
+            }
+    }
+
+}
+
