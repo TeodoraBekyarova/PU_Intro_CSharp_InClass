300f2e35efccca00c0fc5e3c1b92c87fcabbe1b5
piryankov <gpiryankov777@gmail.com>
2018-11-13 14:13:15 +0200

    2031

---
+++ W3InClass/2031(2,8,16)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2031_2_8_16_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Числото 2031 представено в различни бройни системи");
+            int value = 2031;
+            string binary2 = Convert.ToString(value, 2);
+            string binary8 = Convert.ToString(value, 8);
+            string binary16 = Convert.ToString(value, 16);
+            Console.WriteLine("Представено в двойчна бройна система:" + binary2);
+            Console.WriteLine("Представено в осмична бройна система:" + binary8);
+            Console.WriteLine("Представено в шестнайсетична бройна система:" + binary16);
+
+        }
+    }
+}
